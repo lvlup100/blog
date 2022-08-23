209cd065ffd2eb860a43aad75fe0ff4df2c381f0
shady-abdelhamid <shady-abdelhamid@users.noreply.github.com>
2022-08-23 00:47:36 +0000

    jekyll build from Action 2b8c8d62d11b6806f4d3892e5ab1979fb268d263

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "مرحباً بك في Jekyll !",
+        "excerpt":"ستجد هذه المشاركة في ملف _posts. قم بتحريره وأعد إنشاء الموقع لمشاهدة التغييرات التي أجريتها. يمكنك إعادة بناء الموقع بعدة طرق مختلفة ، ولكن الطريقة الأكثر شيوعًا هي تشغيل jekyll serve ، والتي تقوم بتشغيل خادم ويب وتجديد موقعك تلقائيًا عند تحديث الملف. Jekyll requires blog post files to be...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/welcome-to-jekyll/",
+        "teaser": null
+      }]
